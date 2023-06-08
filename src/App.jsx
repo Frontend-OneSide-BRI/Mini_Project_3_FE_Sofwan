8d7933686188aec9e5d859fe2aa3e4933023b654
Sofwan86 <45458797+Sofwan86@users.noreply.github.com>
2023-06-08 22:11:48 +0700

    fix padding banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
