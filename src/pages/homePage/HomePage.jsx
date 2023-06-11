764a37cb06f092f0d5849e2183538903e4663a7f
Sofwan86 <45458797+Sofwan86@users.noreply.github.com>
2023-06-11 22:26:34 +0700

    Update HomePage.jsx

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './style.scss'
 import Banners from '../../components/organisms/headers/banners/Banners'
 import Popular from '../../components/organisms/movieList/popular/Popular'
+import TopRated from '../../components/organisms/topRated/TopRated'
 const HomePage = () => {
   return (
     <div className='homePage'>
         <Banners/>
         <Popular/>
-        <div style={{height:1000}}></div>
+        <TopRated/>
+        {/* <div style={{height:1000}}></div> */}
     </div>
   )
 }
